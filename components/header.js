d65ebe789c138258e6b666b1c672e75c6cffbc75
Kamen Tsvetkov <kntsvetkov@gmail.com>
2024-11-20 20:36:51 -0800

    Add header to page layout

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Header.module.css';
+
+export default function Header() {
+    return (
+        <div className={styles.header_container}>
+            <h1>Pokemon Stop</h1>
+        </div>
+      )
+}
--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,11 @@
 import CustomNavbar from './navbar'
 import Footer from './footer'
+import Header from './header'
  
 export default function Layout({ children }) {
   return (
     <>
+      <Header />
       <CustomNavbar />
       <main>{children}</main>
       <Footer />
