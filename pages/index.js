3f8581e30656e31afec85adb9fb6a9c9a2900a2a
Kamen Tsvetkov <kntsvetkov@gmail.com>
2024-11-21 22:55:12 -0800

    Fix some home page styling

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,23 @@ export default function Home() {
 
       <main>
         <h3>Home</h3>
-        <div className={styles.text_container}>
-          Welcome to Pokemon Stop!
-          <br/><br/>
+        <div className={styles.content_container}>
+          <div className={styles.text_container}>
+            Welcome to Pokemon Stop!
+            <br/><br/>
+          </div>
           <div className={styles.pikachu_img_container}>
             <img className={styles.pikachu_img} src='/detective_pikachu.png'></img>
           </div>
-          <br/><br/>
-          Help detective Pikachu give out speeding tickets to keep the roads safe.
-          <br/><br/><br/>
-          - Tip 1: Stop as many Pokemon as you can to get a new high score! 
-          <br/><br/><br/>
-          - Tip 2: Click only on the Pokemon that are speeding or else it is GAME OVER. 
-          <br/><br/>
+          <div className={styles.text_container}>
+            <br/><br/>
+            Help detective Pikachu give out speeding tickets to keep the roads safe.
+            <br/><br/><br/>
+            - Tip 1: Stop as many Pokemon as you can to get a new high score! 
+            <br/><br/><br/>
+            - Tip 2: Click only on the Pokemon that are speeding or else it is GAME OVER. 
+            <br/><br/>
+          </div>
           <div className={styles.home_links_container}>
             <Link className={styles.home_link} href="/traffic-stops">Play Game</Link>
             <Link className={styles.home_link} href="/pokemon-directory">Research Pokemon</Link>
